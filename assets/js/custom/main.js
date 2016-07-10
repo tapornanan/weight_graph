dc6f99284f5cc1a1122d80555dda82ff447d0d97
tapornanan <tapornanan23@gmail.com>
2016-07-10 19:18:52 +0700

    fixed function find diff_date

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -34,6 +34,10 @@ $(document).ready(function(){
 
   });
 
+
+
+
+
   var myChart = new Chart(ctx, {
     type: 'line',
 
@@ -99,91 +103,47 @@ $(document).ready(function(){
 
 
 function compare_date(_data) {
-  var _date = moment();
-  console.log("now:");
-  console.log(_date);
-  console.log(_date.date());
-  console.log(_date.month());
-  console.log(_date.year());
+  var now   = moment();
+  var date  = moment().date();
+  var year  = moment().year();
+
   var subtract_one_month = moment().subtract(1, 'months');
-  var year = moment().year();
   var month = subtract_one_month.month();
 
-  var _temp = {
-    'year' : year,
-    'month': month
-  };
+  var past  = moment().set({'year': year, 'month': month, 'date': date });
+  console.log(past);
+  console.log("to");
+  console.log(now);
 
-  // created date for using in comparision
-  var created_date = moment().set(_temp);
-  console.log(created_date.date());
-  console.log(created_date.month());
-  console.log(created_date.year());
-  console.log("> (-1) month : <");
-  console.log(created_date);
-
-  var end = created_date.endOf('month');
-  console.log("> the ending of the month <");
-  console.log(end);
-
-  var _temp_start = created_date.date() +"/"+ created_date.month()  +"/"+ created_date.year();
-  var _temp_now   = moment().date() +"/"+ moment().month()  +"/"+ moment().year();
-  console.log(_temp_start);
-  console.log(_temp_now);
-
-  var a = moment(_temp_start, 'DD/MM/YYYY');
-  var b = moment(_temp_now, 'DD/MM/YYYY');
-  var days = b.diff(a, 'days')
-  console.log(days);
-
-  var temp_past = created_date;
-  for (var k = 0; k < 31 ; k++) {
-    if ( _date >= created_date ){
-      console.log(k);
+  var diff_duration = now.diff(past, 'days');
+  console.log(diff_duration);
 
 
+  var temp_from = past ;
+  for (var k = 0; k < diff_duration+2 ; k++) {
+    var _months = temp_from.month();
+    var _date   = temp_from.date();
+    console.log(_months);
+    console.log(_date);
+    for (var i = 0; i < _data.length; i++) {
+      // each user.
+      // get name here!
+      console.log(_data[i].name);
 
+      for (var j = 0; j < _data[i].weight.length; j++) {
+        var weight = _data[i].weight[j];
+        var date_time = new Date(_data[i].weight[j].createdAt);
+        // console.log("Date create for this weight is : ");
+        // console.log(date_time);
 
+        var temp_date  = date_time.getDate();
+        var temp_month = date_time.getMonth();
 
-
-     temp_past  = temp_past.add(1, 'days');
-     console.log(temp_past);
-    }
-  }
-
-
-
-
-  for (var i = 0; i < _data.length; i++) {
-
-    console.log(_data[i].name);
-    var temp_weight ;
-    for (var j = 0; j < _data[i].weight.length; j++) {
-      var weight = _data[i].weight[j];
-      var date_time = new Date(_data[i].weight[j].createdAt);
-      console.log("Date create for this weight is : ");
-      console.log(date_time);
-
-      var temp_date  = date_time.getDate();
-      var temp_month = date_time.getMonth();
-      console.log("record date:");
-      console.log(temp_date);
-      console.log("record month:");
-      console.log(temp_month);
-
-      if ( month == temp_month ) {
-        if ( date == temp_date ){
-          console.log("> record and date are equal. <");
-
-
-
+        if ( temp_month == _months && temp_date == _date ) {
+          console.log("loop date and data date are equal! yeaH..");
         }
       }
-
-
-
-
-
     }
+    temp_from = temp_from.add(1, 'days');
   }
 }
